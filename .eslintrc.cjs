444b7f4830c6dadd0019f653fed0a00210943c34
ilhamdhiya01 <ulhaqilhamdhiya@gmail.com>
2025-02-12 15:42:07 +0700

    test: setup husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-refresh", "@typescript-eslint", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-300">
+      <button type="button" className="btn">
+        Button
+      </button>
+      <button type="button" className="btn btn-neutral">
+        Neutral
+      </button>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-accent">
+        Accent
+      </button>
+      <button type="button" className="btn btn-ghost">
+        Ghost
+      </button>
+      <button type="button" className="btn btn-link">
+        Link
+      </button>
+    </div>
+  );
+}
+
+export default App;
